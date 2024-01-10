752533f54681ba8fc3a2a7390dc4ac4df70ecf97
Tom Saillard <tom.saillard@ynov.com>
2024-01-10 18:08:42 +0100

    implémentation de l'api wweather sur la page actu, manque juste le CSS actuellement, ajout de la function calculposition

--- Front/js/Random.js
+++
@@ -1,9 +0,0 @@
-function randomInt(min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
-// Générer un nombre aléatoire à 5 chiffres
-let randomNumber = randomInt(10000, 99999);
-
-// Sélectionner l'élément avec l'ID "random-number" et mettre le nombre aléatoire à l'intérieur
-document.getElementById("random-number").textContent = randomNumber;
---
+++ Front/js/weatherApi.js
@@ -0,0 +1,77 @@
+
+function getWeatherByLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(success, error);
+    } else {
+        console.log("La géolocalisation n'est pas supportée par ce navigateur.");
+    }
+}
+
+function getYourWeatherATHouse() {
+    if (navigator.house) {
+        console.log("La géocalisation est disponible pour votre maison")
+
+        navigator.house.getCurrentPosition(success, error);
+    } else {
+        console.log("La géolocalisation n'est pas disponivle pour votre maison");
+    }
+}
+
+function CalculPositions(positionX, positionY) {
+    // Vérifier si les valeurs sont numériques et non négatives
+    if (typeof adjacent !== 'number' || typeof hypotenuse !== 'number' || adjacent < 0 || hypotenuse <= 0) {
+        return "Veuillez entrer des valeurs numériques positives";
+    }
+
+    const Position = positionX / positionY;
+    return Position;
+}
+//Exemple utilisation
+const positionX = 3;
+const positionY = 5;
+
+const CalculPosition = calculerCosinus(adjacentSide, hypotenuseLength);
+console.log("Le cosinus de l'angle est:", cosinusAngle);
+
+
+
+function success(position) {
+    const latitude = position.coords.latitude;
+    const longitude = position.coords.longitude;
+    const apiKey = '480c84fa294455c1d623e90273595658'; 
+
+    window.fetch(`http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&lang=fr&units=metric&appid=${apiKey}`)
+        .then(res => res.json())
+        .then(resJson => {
+            const temperatureElement = document.getElementById('temperature');
+            const temperatureMaxElement = document.getElementById('temperatureMax');
+            const temperatureMinElement = document.getElementById('temperatureMin');
+            const weatherIconElement = document.getElementById('weatherIcon');
+            
+
+            temperatureElement.textContent = `Température : ${resJson.main.temp}°C`;
+            temperatureMaxElement.textContent = `TemperatureMax : ${resJson.main.temp_max}°C`;
+            temperatureMinElement.textContent = `TemperatureMin : ${resJson.main.temp_min}°C`;
+
+            function getWeatherIconURL(weatherCode) {
+                switch (weatherCode) {
+                    case 'Clear':
+                        return '../images/soleil.png';
+                    case 'Clouds':
+                        return '../images/nuage.png';
+                    default:
+                        return '../images/pluie.png';
+                }
+            }
+
+            const weatherIconURL = getWeatherIconURL(resJson.weather[0].main);
+            weatherIconElement.src = weatherIconURL;
+        })
+        .catch(error => {
+            console.log("Erreur lors de la récupération des données :", error);
+        });
+}
+
+function error(error) {
+    console.log('Erreur de géolocalisation : ', error);
+}
