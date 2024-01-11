a173d8ddc1c8d9cdbfa79f4b914caef696ffc149
Tilio <tilio.bourigault@ynov.com>
2024-01-11 13:39:34 +0100

    fix all css of header

--- Front/js/weatherApi.js
+++ Front/js/weatherApi.js
@@ -1,4 +1,3 @@
-
 function getWeatherByLocation() {
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(success, error);
@@ -9,35 +8,17 @@ function getWeatherByLocation() {
 
 function getYourWeatherATHouse() {
     if (navigator.house) {
-        console.log("La géocalisation est disponible pour votre maison")
-
+        console.log("La géocalisation est disponible pour votre maison");
         navigator.house.getCurrentPosition(success, error);
     } else {
         console.log("La géolocalisation n'est pas disponivle pour votre maison");
     }
 }
 
-function CalculPositions(positionX, positionY) {
-    // Vérifier si les valeurs sont numériques et non négatives
-    if (typeof positionX !== 'number' || typeof positionY !== 'number' || positionX < 0 || positionY <= 0) {
-        return "Veuillez entrer des valeurs numériques positives";
-    }
-
-    const Position = positionX / positionY;
-    return Position;
-}
-//Exemple utilisation
-const positionX = 3;
-const positionY = 5;
-
-const CalculPosition = calculerCosinus(positionX, positionY);
-
-
-
 function success(position) {
     const latitude = position.coords.latitude;
     const longitude = position.coords.longitude;
-    const apiKey = '480c84fa294455c1d623e90273595658'; 
+    const apiKey = '480c84fa294455c1d623e90273595658';
 
     window.fetch(`http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&lang=fr&units=metric&appid=${apiKey}`)
         .then(res => res.json())
@@ -46,7 +27,7 @@ function success(position) {
             const temperatureMaxElement = document.getElementById('temperatureMax');
             const temperatureMinElement = document.getElementById('temperatureMin');
             const weatherIconElement = document.getElementById('weatherIcon');
-            
+            const weatherButton = document.getElementById('weatherButton');
 
             temperatureElement.textContent = `Température : ${resJson.main.temp}°C`;
             temperatureMaxElement.textContent = `TemperatureMax : ${resJson.main.temp_max}°C`;
@@ -55,16 +36,18 @@ function success(position) {
             function getWeatherIconURL(weatherCode) {
                 switch (weatherCode) {
                     case 'Clear':
-                        return '../images/soleil.png';
+                        return '../assets/images/soleil.png';
                     case 'Clouds':
-                        return '../images/nuage.png';
+                        return '../assets/images/nuage.png';
                     default:
-                        return '../images/pluie.png';
+                        return '../assets/images/pluie.png';
                 }
             }
 
             const weatherIconURL = getWeatherIconURL(resJson.weather[0].main);
             weatherIconElement.src = weatherIconURL;
+
+            weatherButton.style.visibility = 'hidden';
         })
         .catch(error => {
             console.log("Erreur lors de la récupération des données :", error);
