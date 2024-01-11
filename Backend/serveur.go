d8977349593c3921eee77b2633ab3ea764785f7d
Tilio <tilio.bourigault@ynov.com>
2024-01-11 16:41:17 +0100

    fix big problem tom and tilio

--- Backend/serveur.go
+++ Backend/serveur.go
@@ -2,11 +2,23 @@ package main
 
 import (
 	"bytes"
+	"encoding/json"
 	"fmt"
 	"html/template"
+	"io/ioutil"
 	"net/http"
 	"path/filepath"
 )
+
+// Post struct represents a blog post
+type Post struct {
+	Author  string `json:"author"`
+	Title   string `json:"title"`
+	Content string `json:"content"`
+	Date    string `json:"date"`
+}
+
+var Posts []Post
 
 func renderTemplate(w http.ResponseWriter, tmpl string, p interface{}) {
 	var myCache, err = createTemplateCache()
@@ -29,24 +41,34 @@ func homeHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func actuHandler(w http.ResponseWriter, r *http.Request) {
-	renderTemplate(w, "homeActu.page.html", nil)
+	renderTemplate(w, "homeActu.page.html", struct{ Posts []Post }{Posts})
 }
 
 func main() {
+	fmt.Println("http://localhost:8080")
+	http.Handle("/assets/", http.StripPrefix("/assets/", http.FileServer(http.Dir("../Front/assets"))))
 	jsHandler := http.FileServer(http.Dir("../Front/js"))
 	http.Handle("/js/", http.StripPrefix("/js/", jsHandler))
-	fs := http.FileServer(http.Dir("../Front/assets"))
-	http.Handle("/assets/", http.StripPrefix("/assets/", fs))
-	fmt.Println("http://localhost:8080")
-	http.HandleFunc("/", homeHandler)
+	loadPostsFromFile()
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == "GET" {
+			homeHandler(w, r)
+		} else if r.Method == "POST" {
+			addPost(w, r)
+		}
+	})
 	http.HandleFunc("/actu", actuHandler)
-	http.ListenAndServe(":8080", nil)
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		fmt.Println("Erreur lors du démarrage du serveur :", err)
+	}
+	savePostsToFile()
 }
 
 func createTemplateCache() (map[string]*template.Template, error) {
 	myCache := map[string]*template.Template{}
 
-	pages, err := filepath.Glob("../Front/Page/*.page.html")
+	pages, err := filepath.Glob("../Front/page/*.page.html")
 	if err != nil {
 		return myCache, err
 	}
@@ -63,4 +85,48 @@ func createTemplateCache() (map[string]*template.Template, error) {
 	}
 
 	return myCache, nil
+}
+
+func loadPostsFromFile() {
+	data, err := ioutil.ReadFile("posts.json")
+	if err != nil {
+		fmt.Println("Erreur lors de la lecture du fichier JSON :", err)
+		return
+	}
+	err = json.Unmarshal(data, &Posts)
+	if err != nil {
+		fmt.Println("Erreur lors de la désérialisation du fichier JSON :", err)
+	}
+}
+
+func savePostsToFile() {
+	data, err := json.Marshal(Posts)
+	if err != nil {
+		fmt.Println("Erreur lors de la sérialisation en JSON :", err)
+		return
+	}
+	err = ioutil.WriteFile("posts.json", data, 0644)
+	if err != nil {
+		fmt.Println("Erreur lors de l'écriture dans le fichier JSON :", err)
+	}
+}
+
+func addPost(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+	author := r.Form.Get("author")
+	title := r.Form.Get("title")
+	content := r.Form.Get("content")
+	date := r.Form.Get("date")
+
+	newPost := Post{
+		Author:  author,
+		Title:   title,
+		Content: content,
+		Date:    date,
+	}
+
+	Posts = append(Posts, newPost)
+	savePostsToFile()
+
+	fmt.Fprintf(w, "Post ajouté avec succès:\nTitle: %s\nContent: %s\nDate: %s\n", title, content, date)
 }
