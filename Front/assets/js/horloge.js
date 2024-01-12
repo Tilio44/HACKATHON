eb4343990d2e624347cc7649cd36e819f7c043b0
Matthias <matthias.naert@ynov.com>
2024-01-12 15:44:57 +0100

    ajout des commentaires pour définir le fichier

--- Front/assets/js/horloge.js
+++ Front/assets/js/horloge.js
@@ -1,3 +1,4 @@
+//  FUNCTION HORLOGE //                                                                                                                                                                                                                             j'aime le veau
 function Eure() {
   var currentDate = new Date();
   var dateTimeString = currentDate.toLocaleString();
